3397118e17b5d091d695c51ecb3b2455312c422a
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-06-01 18:16:49 +0500

    initializing nest auth/user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.setGlobalPrefix('api')
+	app.use(cookieParser())
+	app.enableCors({
+		origin: ['http://localhost:3300'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	})
+	await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
