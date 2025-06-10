18caef16d64c1fc370a8f49cca2c7fff18e62317
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-06-10 16:46:52 +0500

    auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,13 @@ import {
 import { JwtService } from '@nestjs/jwt'
 import { UserService } from 'src/user/user.service'
 import { AuthDto } from './dto/auth.dto'
+import { Response } from 'express'
 import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
 	constructor(
 		private jwt: JwtService,
 		private userService: UserService
@@ -32,6 +35,20 @@ export class AuthService {
 		if (oldUser) throw new BadRequestException('User already exists.')
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		const { password, ...user } = await this.userService.create(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return {
+			user,
+			...tokens
+		}
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		if (!result) throw new UnauthorizedException('Invalid refresh token.')
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-unsafe-member-access
+		const { ...user } = await this.userService.getById(result.id)
 		const tokens = this.issueTokens(user.id)
 
 		return {
@@ -63,5 +80,28 @@ export class AuthService {
 		if (!isValid) throw new UnauthorizedException('Invalid password.')
 
 		return user
+	}
+
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: expiresIn,
+			secure: true,
+			sameSite: 'none'
+		})
+	}
+
+	removeRefreshTokenFromResponse(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: new Date(0),
+			secure: true,
+			sameSite: 'none'
+		})
 	}
 }
